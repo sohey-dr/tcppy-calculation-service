61c270126ec4b7d1cae88a42dc72342530710b02
soheyHashimoto <heyso1209@gmail.com>
2022-08-20 17:41:03 +0900

    [ADD]fraction impl

--- src/domain/model/fraction.rs
+++ src/domain/model/fraction.rs
@@ -2,3 +2,20 @@ pub struct fraction {
     numerator: i32,
     denominator: i32,
 }
+
+impl fraction {
+    pub fn new(numerator: i32, denominator: i32) -> fraction {
+        fraction {
+            numerator,
+            denominator,
+        }
+    }
+
+    pub fn numerator(&self) -> i32 {
+        self.numerator
+    }
+
+    pub fn denominator(&self) -> i32 {
+        self.denominator
+    }
+}
