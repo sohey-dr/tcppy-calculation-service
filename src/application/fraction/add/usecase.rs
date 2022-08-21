7965cd8b92e48ae63b8ad10172de0fbbf088e9ce
soheyHashimoto <heyso1209@gmail.com>
2022-08-22 03:05:48 +0900

    [MODIFY]最初に約分

--- src/application/fraction/add/usecase.rs
+++ src/application/fraction/add/usecase.rs
@@ -6,9 +6,23 @@ pub fn exec() -> FractionAddUsecaseOutput {
   let f2 = fraction::new_random_fraction();
   let added = fraction::add(f1, f2);
 
+  let f1_reduced = if !f1.eq(&f1.reduce()) {
+    f1.reduce()
+  } else {
+    f1
+  };
+  let f2_reduced = if !f2.eq(&f2.reduce()) {
+    f2.reduce()
+  } else {
+    f2
+  };
+
+  let mut calculation_process = fraction::reduce_denominators(vec![f1_reduced, f2_reduced]);
+
   let reduced = added.reduce();
 
   let result = if reduced.denominator() == 1 {
+    calculation_process.push(reduced.to_string());
     format!("{}", reduced.numerator())
   } else {
     reduced.to_string()
@@ -17,6 +31,6 @@ pub fn exec() -> FractionAddUsecaseOutput {
   return FractionAddUsecaseOutput {
     calculation_formula: vec![f1.to_string(), "+".to_string(), f2.to_string()],
     result: result,
-    calculation_process: vec!["1/2".to_string()],
+    calculation_process: calculation_process,
   };
 }
