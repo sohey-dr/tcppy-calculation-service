05c4010795d0f9e3ffa503ce1949b98bdeeacc99
soheyHashimoto <heyso1209@gmail.com>
2022-08-22 01:20:08 +0900

    [MODIFY]配列に修正

--- src/adapter/presenter/fraction/add_json_presenter.rs
+++ src/adapter/presenter/fraction/add_json_presenter.rs
@@ -1,19 +1,13 @@
 use actix_web::{HttpResponse, Responder};
 use serde::Serialize;
 
-use crate::domain::model::fraction;
 use crate::application::fraction::add::usecase_output::FractionAddUsecaseOutput;
 
 #[derive(Serialize)]
 struct FractionAddJsonResponse {
   calculation_formula: String,
   result: String,
-  calculation_process: Vec<FractionAddCalculationProcess>,
-}
-
-#[derive(Serialize)]
-struct FractionAddCalculationProcess {
-  process: String,
+  calculation_process: Vec<String>,
 }
 
 pub fn exec(output: FractionAddUsecaseOutput) -> impl Responder {
@@ -21,15 +15,9 @@ pub fn exec(output: FractionAddUsecaseOutput) -> impl Responder {
 
   let result = output.result;
 
-  let calculation_process = vec![
-    FractionAddCalculationProcess {
-      process: "1/2".to_string(),
-    }
-  ];
-
   return HttpResponse::Ok().json(FractionAddJsonResponse {
     calculation_formula: calculation_formula,
     result: result,
-    calculation_process: calculation_process,
+    calculation_process: output.calculation_process,
   });
 }
